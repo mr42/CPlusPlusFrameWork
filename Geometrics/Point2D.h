9a23faab95f9271c024ba8b38c0b393256c78297
Michael <michael@ubuntu.(none)>
2014-02-15 21:23:45 +0100

    Add a 2 dimensional point class

---
+++ Geometrics/Point2D.h
@@ -0,0 +1,27 @@
+// Copyright 10. Februar 2014 University of Freiburg
+// Author michael <rudolphmichael42@gmail.com>
+
+#ifndef GEOMETRICS_POINT2D_H_
+#define GEOMETRICS_POINT2D_H_
+
+namespace Geometrics {
+  template<class T>
+  class Point2D {
+    public:
+    Point2D(const T& x, const T& y) {
+      _x = x;
+      _y = y;
+    }
+    // Two Points are equal if both the x and the y
+    // coordinate are the same.
+    bool operator==(const Point2D p) {
+      return this->_x == p._x && this->_y == p._y;
+    }
+
+    T _x;
+    T _y;
+  };
+}
+
+#endif  // GEOMETRICS_POINT2D_H_
+
